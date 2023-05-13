62f65d7aa2ace982b23b4d8b2045d5eae6649cb1
showoi <96547023+showoi@users.noreply.github.com>
2023-05-13 15:51:58 +0300

    .

---
+++ en_script.py
@@ -0,0 +1,13 @@
+def main():
+    print ("""
+    
+           _              _ _    _ _   
+          | |            | | |  (_) |  
+ ___ _   _| |_ ___   ___ | | | ___| |_ 
+/ __| | | | __/ _ \ / _ \| | |/ / | __|
+\__ \ |_| | || (_) | (_) | |   <| | |_ 
+|___/\__,_|\__\___/ \___/|_|_|\_\_|\__|
+    """)
+
+print("Please choose one of the options below.")
+print("1) Energy output calculator")
--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-#import-necessary-libraries--------------#
-import json
-#language-declaration--------------------#
-with open('languages.json', 'r') as f:
-    translations = json.load(f)
-
+import tr_script, en_script
 
 while True:
     print("Which language would you like to use?")
@@ -19,21 +14,4 @@ while True:
     else:       
         print("Invalid choice, please try again or press (ctrl+c) to exit.\n \n")
         print(chr(27) + "[2J")
-        continue
-
-#----------------------------------------#
-def menu():
-    print("""
-               __                   ___    __          __      
-              /\ \__               /\_ \  /\ \      __/\ \__   
-  ____  __  __\ \ ,_\   ___     ___\//\ \ \ \ \/'\ /\_\ \ ,_\  
- /',__\/\ \/\ \\ \ \/  / __`\  / __`\\ \ \ \ \ , < \/\ \ \ \/  
-/\__, `\ \ \_\ \\ \ \_/\ \L\ \/\ \L\ \\_\ \_\ \ \\`\\ \ \ \ \_ 
-\/\____/\ \____/ \ \__\ \____/\ \____//\____\\ \_\ \_\ \_\ \__\
- \/___/  \/___/   \/__/\/___/  \/___/ \/____/ \/_/\/_/\/_/\/__/
-                                                               
-                                                               
-""")
-    print("1. Script 1")
-    print("2. Script 2")
-    print("3. Script 3")
+        continue 
